2ff10338e6df56b2fcafa482d729830458e9eddc
html546 <1776787598@qq.com>
2019-04-26 08:24:47 +0800

    我的页面

--- src/page/Mine.vue
+++ src/page/Mine.vue
@@ -1,17 +1,46 @@
 <template>
-  <div>
-    <h1>我是我的</h1>
+  <div class="mine">
+    <van-nav-bar class="mine_bar" title="我的" :border="false"></van-nav-bar>
+    <div class="mine_message">
+      <img class="avatar" src="../assets/avatar.png" width="56" height="56" alt>
+    </div>
   </div>
 </template>
 
 <script>
+import { NavBar, Cell, CellGroup } from "vant";
 export default {
   name: "",
   data() {
     return {};
+  },
+  components: {
+    "van-nav-bar": NavBar,
+    "van-cell": Cell,
+    "van-cell-group": CellGroup
   }
 };
 </script>
 
 <style lang="less" scoped>
+.mine {
+  width: 100%;
+  height: 100%;
+  background: #000f26;
+  .mine_bar {
+    background: #0b1a3b;
+  }
+  .mine_message {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+    height: 80px;
+    width: 100%;
+    padding: 0 16px;
+    background: #0b1a3b;
+    .avatar {
+    }
+  }
+}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,10 @@ module.exports = {
           'nav-bar-background-color': '#181b2c',
           'nav-bar-icon-color': '#fff',
           'nav-bar-text-color': '#fff',
-          'nav-bar-title-text-color': '#fff'
+          'nav-bar-title-text-color': '#fff',
+          'cell-background-color': "#0b1a3b",
+          'cell-text-color': "#fff",
+          'cell-active-color': '#0b1a3b'
         }
       }
     }
